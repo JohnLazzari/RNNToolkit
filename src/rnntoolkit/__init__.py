6221b724cc9c38807443e326a349ae9789fbf34b
John Lazzari <lazzarijohn2@gmail.com>
2026-03-19 15:16:13 -0400

    added base classes to import structure

--- src/rnntoolkit/__init__.py
+++ src/rnntoolkit/__init__.py
@@ -1,13 +1,17 @@
 from .fixed_points.fp_finder import FixedPointFinder
+from .fixed_points.fp_finder_base import FixedPointFinderBase
 from .fixed_points.fp import FixedPointCollection
 from .flow_fields.flow_field_finder import FlowFieldFinder
+from .flow_fields.flow_field_finder_base import FlowFieldFinderBase
 from .flow_fields.flow_field import FlowField
 from .linear import Linearization
 
 __all__ = [
     "FixedPointFinder",
+    "FixedPointFinderBase",
     "FixedPointCollection",
     "FlowFieldFinder",
+    "FlowFieldFinderBase",
     "FlowField",
     "Linearization",
 ]
